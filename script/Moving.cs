e3c93129211507d6cad3d29e004ac967837b9199
Steven Kim <steven3442@gmail.com>
2021-09-15 09:26:31 -0400

    Modified with Godot v3.3

---
+++ script/Moving.cs
@@ -0,0 +1,46 @@
+/*************************************************************************/
+/*  Moving.cs                                                            */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           DungeonPath                                 */
+/*             https://github.com/inthelamp/dungeonpath                  */
+/*************************************************************************/
+/* Copyright (c) 2018-2019 Dong Won Kim.                                 */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+using Godot;
+using System;
+
+public abstract class Moving : RigidBody2D
+{
+	public Sprite MovingSprite { get; set; } //Moving Object Sprite
+	public AnimationPlayer AnimatPlay { get; set; } //AnimationPlayer
+	public string Animation { get; set; } //Animation like "idle", "walk", "hit", "up", "down"
+	public PathFollow2D SpawnLocation { get; set; }
+
+	public int MinSpeed { get; set; } //Minimum speed
+	public int MaxSpeed { get; set; } //Maximum speed
+
+	public String[] MovingTypes; //Define moving animations
+
+	//Start moving
+	public abstract void Start();
+}
